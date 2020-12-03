69c73d4c816a6c764cfd17842374c0ae8059e35e
Andy Rose <andyrose@hey.com>
2020-12-02 21:08:02 -0600

    feat(form-field): create init library for form-field

---
+++ projects/form/form-field/src/lib/form-field.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FormFieldComponent } from './form-field.component';
+
+
+
+@NgModule({
+  declarations: [FormFieldComponent],
+  imports: [
+  ],
+  exports: [FormFieldComponent]
+})
+export class FormFieldModule { }

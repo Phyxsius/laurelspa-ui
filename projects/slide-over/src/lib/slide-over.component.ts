cee951d8b0046d4edabace645b8e814a27567247
Andy Rose <andyrose@hey.com>
2020-12-15 15:59:49 -0600

    feat(slide-over): init Slide Over element

---
+++ projects/slide-over/src/lib/slide-over.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ls-slide-over',
+  template: `
+    <p>
+      slide-over works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SlideOverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/slide-over/src/lib/slide-over.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SlideOverComponent } from './slide-over.component';
+
+
+
+@NgModule({
+  declarations: [SlideOverComponent],
+  imports: [
+  ],
+  exports: [SlideOverComponent]
+})
+export class SlideOverModule { }
